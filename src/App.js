80020b60ecdd9ad9a5fbe49b691251c320363e6b
Cleveland33 <77119527+Cleveland33@users.noreply.github.com>
2021-04-07 07:34:12 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import NavBar from './components/navBar';
+import Footer from './components/footer';
+import Catalog from './components/catalog';
+import Home from "./components/home";
+import About from "./components/about";
+import Cart from "./components/cart";
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <NavBar></NavBar>
+
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/home" exact component={Home} ></Route>
+        <Route path="/catalog" exact component={Catalog} ></Route>
+        <Route path="/about" exact component={About}></Route> 
+        <Route path="/cart" exact component={Cart}></Route>     
+      </Switch>
+
+
+
+        <Footer></Footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
